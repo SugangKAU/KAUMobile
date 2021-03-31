e3d364ad381433e7a0d379c6a83d8f148a58564b
Lagnesia <sjwkon619@naver.com>
2021-03-31 23:05:14 +0900

    수강 관련 레이아웃
    수강과목-Class 노트작성 - Note 과제 및 업로드 - Assign

---
+++ app/src/main/java/com/example/sswolf/kausugang/NoteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sswolf.kausugang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note)
+    }
+}
