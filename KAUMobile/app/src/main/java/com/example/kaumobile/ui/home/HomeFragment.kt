af875501881065c9a497c6382cc6dba95155de0a
dkfma0605 <80970930+dkfma0605@users.noreply.github.com>
2021-04-29 00:54:44 +0900

    강의 삭제 기능 추가
    1. 강의 삭제 기능 구현
    2. 강의가 없을 때 빈 버튼 추가

--- KAUMobile/app/src/main/java/com/example/kaumobile/ui/home/HomeFragment.kt
+++ KAUMobile/app/src/main/java/com/example/kaumobile/ui/home/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.DialogInterface
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,9 +20,6 @@ import com.example.kaumobile.firebase.Database
 class HomeFragment : Fragment() {
     private var year = 2021
     private var semester = 1
-    private var weekNum = 0
-    private var startTimeNum = 0
-    private var endTimeNum = 0
     private var classNum = 0
     private val weekList = arrayOf("월요일","화요일","수요일","목요일","금요일")
     private val weekList2 = arrayOf("월","화","수","목","금")
@@ -41,11 +40,20 @@ class HomeFragment : Fragment() {
 
         val buttonView = root.findViewById<LinearLayout>(R.id.button_view)
         val dynamicClass = Array(20){Button(requireContext())}
+        val addRequest = Button(requireContext())
         val layoutParams = LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT
         )
         layoutParams.setMargins(changeDP(10), 0, changeDP(10), 0)
+
+        addRequest.layoutParams = layoutParams
+        addRequest.setText("강의를 추가해주세요")
+        addRequest.width = changeDP(170)
+        addRequest.setBackgroundColor(Color.parseColor("#808080"))
+        addRequest.setTextColor(Color.parseColor("#FFFFFF"))
+        addRequest.gravity = Gravity.CENTER
+        buttonView.addView(addRequest)
 
         root.findViewById<TextView>(R.id.text_main_semester).setText(year.toString() + "년도 " + semester.toString() + "학기")
 
@@ -70,6 +78,10 @@ class HomeFragment : Fragment() {
         }
 
         root.findViewById<View>(R.id.button_main_addclass).setOnClickListener {
+            var weekNum = 0
+            var startTimeNum = 0
+            var endTimeNum = 0
+
             var builder = AlertDialog.Builder(requireContext())
             builder.setTitle("강의 추가")
 
@@ -85,16 +97,56 @@ class HomeFragment : Fragment() {
                 Database().addNewSubject("${edit1?.text}", "${edit2?.text}", "${edit3?.text}",
                     weekList2[weekNum]+timeList2[startTimeNum]+timeList2[endTimeNum])
 
+                if(classNum == 0)
+                    buttonView.removeView(addRequest)
+
                 dynamicClass[classNum].layoutParams = layoutParams
+                dynamicClass[classNum].id = classNum
                 dynamicClass[classNum].setText("${edit1?.text}\n\n${edit2?.text}\n\n${edit3?.text}\n\n" +
                         "${weekList[weekNum]} ${timeList[startTimeNum]} ~ ${timeList[endTimeNum]}")
                 dynamicClass[classNum].width = changeDP(170)
                 dynamicClass[classNum].setBackgroundColor(Color.parseColor(colorList[classNum]))
                 dynamicClass[classNum].setTextColor(Color.parseColor("#FFFFFF"))
                 buttonView.addView(dynamicClass[classNum])
-                classNum++
 
-                weekNum = 0; startTimeNum = 0; endTimeNum = 0
+                dynamicClass[classNum].setOnClickListener {
+                    Navigation.findNavController(root).navigate(R.id.action_navigation_home_to_navigation_classnote)
+                }
+                dynamicClass[classNum].setOnLongClickListener {
+                    var builder = AlertDialog.Builder(requireContext())
+                    builder.setTitle("강의 삭제")
+
+                    var v1 = layoutInflater.inflate(R.layout.delete_dialog, null)
+                    builder.setView(v1)
+
+                    var listener2 = DialogInterface.OnClickListener { p0, p1 ->
+                        val num = it.id
+                        buttonView.removeView(it)
+                        for(i in num+1..classNum-1) {
+                            dynamicClass[i - 1] = dynamicClass[i]
+                            dynamicClass[i - 1].setBackgroundColor(Color.parseColor(colorList[i - 1]))
+                        }
+                        dynamicClass[classNum-1] = Button(requireContext())
+                        classNum--
+                        //Log.d("태그", "내용 : "+ num + " " + classNum)
+                        if(classNum == 0){
+                            addRequest.layoutParams = layoutParams
+                            addRequest.setText("강의를 추가해주세요")
+                            addRequest.width = changeDP(170)
+                            addRequest.setBackgroundColor(Color.parseColor("#808080"))
+                            addRequest.setTextColor(Color.parseColor("#FFFFFF"))
+                            addRequest.gravity = Gravity.CENTER
+                            buttonView.addView(addRequest)
+                        }
+                    }
+
+                    builder.setPositiveButton("삭제", listener2)
+                    builder.setNegativeButton("취소", null)
+
+                    builder.show()
+                    true
+                }
+                classNum++
             }
 
             val weekSpinner = v1.findViewById<Spinner>(R.id.spinner_week_add)
@@ -131,24 +183,6 @@ class HomeFragment : Fragment() {
             builder.setNegativeButton("취소", null)
 
             builder.show()
-        }
-
-        for(i in 0..classNum-1) {
-            dynamicClass[i].setOnLongClickListener {
-                var builder = AlertDialog.Builder(requireContext())
-                builder.setTitle("강의 삭제")
-
-                var v1 = layoutInflater.inflate(R.layout.delete_dialog, null)
-                builder.setView(v1)
-
-                var listener2 = DialogInterface.OnClickListener { p0, p1 -> }
-
-                builder.setPositiveButton("삭제", listener2)
-                builder.setNegativeButton("취소", null)
-
-                builder.show()
-                true
-            }
         }
 
         return root
